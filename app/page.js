60f25b49f38fe0abe478c1dffd33a8392fdcdce8
josephkhalilkafeke <joseph.kafeke@edu.sait.ca>
2025-09-24 18:45:57 -0600

    Update root page with link to Week 2

--- app/page.js
+++ app/page.js
@@ -2,17 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main style={{ padding: "2rem", textAlign: "center" }}>
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      
+
       <p>
-        <Link href="/week-2" style={{ color: "blue", textDecoration: "underline" }}>
-          Go to Week 2 Page
-        </Link>
+        Go to <Link href="/week-2">Week 2 Page</Link>
       </p>
 
       <p>Additional Information: This page links to week-2 assignments.</p>
     </main>
   );
 }
+
 
